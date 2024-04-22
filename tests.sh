41a78a2a55f1a2e98a5ca26bda9a91ec6415c8d6
Saikishore Mettupalli <saikrmet@gmail.com>
2024-04-21 21:59:29 -0500

    sample2 working

--- tests.sh
+++ tests.sh
@@ -3,3 +3,6 @@ make
 
 /clear/courses/comp421/pub/bin/mkyfs
 /clear/courses/comp421/pub/bin/yalnix -ly 10 yfs samples-lab3/sample1
+
+/clear/courses/comp421/pub/bin/mkyfs
+/clear/courses/comp421/pub/bin/yalnix -ly 10 yfs samples-lab3/sample2
